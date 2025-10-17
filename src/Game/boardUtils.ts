4660ec110995a7004631cab6410bb345c7bba568
Orjan De Smet <orjandesmet@users.noreply.github.com>
2025-10-17 20:59:31 +0200

    replace toSorted by filters

--- src/Game/boardUtils.ts
+++ src/Game/boardUtils.ts
@@ -3,55 +3,63 @@ import { EMPTY, type Board, type CellKey, type CellValue, type FlatBoard } from 
 import { cellUtils } from './cellUtils';
 
 function createBoard(rows: number, columns: number, colors: CellValue[], rng: PRNG): Board {
-      return Array.from({length: columns})
-        .map((_,columnIdx) => Array.from({length: rows})
-          .map((_,rowIdx) => ({
-            key: cellUtils.createCellKey(rowIdx, columnIdx),
-            value: colors[rng.nextRange(0, colors.length)]
-          })
-        ));
-    }
+  return Array.from({ length: columns })
+    .map((_, columnIdx) => Array.from({ length: rows })
+      .map((_, rowIdx) => ({
+        key: cellUtils.createCellKey(rowIdx, columnIdx),
+        value: colors[rng.nextRange(0, colors.length)]
+      })
+      ));
+}
 
 function toString(board: Board): string {
-    const flippedBoard = Array.from({length: board[0].length})
-    .map((_, rowIdx) => Array.from(({length: board.length}))
-    .map((_, columnIdx) => board[columnIdx][rowIdx]));
-    return flippedBoard.map((column) => column.map((cell) => cell.value).join('|')).join('\n');
-    
-  }
+  const flippedBoard = Array.from({ length: board[0].length })
+    .map((_, rowIdx) => Array.from(({ length: board.length }))
+      .map((_, columnIdx) => board[columnIdx][rowIdx]));
+  return flippedBoard.map((column) => column.map((cell) => cell.value).join('|')).join('\n');
+
+}
 
 function getGroup(board: Board, rowIdx: number, columnIdx: number, color: CellValue, visited: CellKey[] = []): CellKey[] {
-    const groupKey = cellUtils.createCellKey(rowIdx, columnIdx);
-    if (visited.includes(groupKey)) {
-      return [];
-    }
-    if (rowIdx >= board[0].length || rowIdx < 0 || columnIdx >= board.length || columnIdx < 0) {
-      return [];
-    }
-    if (board[columnIdx][rowIdx].value !== color) {
-      return [];
-    }
-    visited.push(groupKey);
-    const group = [groupKey]
-      .concat(getGroup(board, rowIdx + 1, columnIdx, color, visited))
-      .concat(getGroup(board, rowIdx - 1, columnIdx, color, visited))
-      .concat(getGroup(board, rowIdx, columnIdx + 1, color, visited))
-      .concat(getGroup(board, rowIdx, columnIdx - 1, color, visited))
+  const groupKey = cellUtils.createCellKey(rowIdx, columnIdx);
+  if (visited.includes(groupKey)) {
+    return [];
+  }
+  if (rowIdx >= board[0].length || rowIdx < 0 || columnIdx >= board.length || columnIdx < 0) {
+    return [];
+  }
+  if (board[columnIdx][rowIdx].value !== color) {
+    return [];
+  }
+  visited.push(groupKey);
+  const group = [groupKey]
+    .concat(getGroup(board, rowIdx + 1, columnIdx, color, visited))
+    .concat(getGroup(board, rowIdx - 1, columnIdx, color, visited))
+    .concat(getGroup(board, rowIdx, columnIdx + 1, color, visited))
+    .concat(getGroup(board, rowIdx, columnIdx - 1, color, visited))
     ;
 
-    return group;
-  }
+  return group;
+}
 
-  function flattenBoard(board: Board): FlatBoard {
-    return board.flatMap((column, columnIdx) => column.map((cell, rowIdx) => ({...cell, rowIdx, columnIdx})));
-  }
+function flattenBoard(board: Board): FlatBoard {
+  return board.flatMap((column, columnIdx) => column.map((cell, rowIdx) => ({ ...cell, rowIdx, columnIdx })));
+}
 
-  function removeGroup(board: Board, group: CellKey[]) {
-    return board
-      .map((column, columnIdx) => column.map((cell, rowIdx) => group.includes(cellUtils.createCellKey(rowIdx, columnIdx)) ? ({...cell, value: EMPTY}) : cell))
-      .map((column) => column.toSorted((leftCell, rightCell) => cellUtils.isEmptyCell(leftCell) ? -1 : cellUtils.isEmptyCell(rightCell) ? 1 : -1))
-      .toSorted((leftColumn) => cellUtils.isEmptyColumn(leftColumn) ? 1 : -1);
-  }
+function removeGroup(board: Board, group: CellKey[]) {
+  const clearedBoard = board
+    .map((column, columnIdx) => column.map((cell, rowIdx) => group.includes(cellUtils.createCellKey(rowIdx, columnIdx)) ? ({ ...cell, value: EMPTY }) : cell))
+    .map((column) => {
+      return [
+        ...column.filter((cell) => cellUtils.isEmptyCell(cell)),
+        ...column.filter((cell) => !cellUtils.isEmptyCell(cell)),
+      ];
+    });
+  return [
+    ...clearedBoard.filter((column) => !cellUtils.isEmptyColumn(column)),
+    ...clearedBoard.filter((column) => cellUtils.isEmptyColumn(column)),
+  ];
+}
 
 
 export const boardUtils = {
