459ccef59ce5bfdf768ba6db7dd6653a21b7434c
Orjan De Smet <orjan.de.smet@gmail.com>
2025-10-24 14:33:50 +0200

    Initial PWA setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['fonts/*', 'creatures/**/*.png'],
+      devOptions: {
+        enabled: false,
+      },
+    }),
+  ],
   define: {
     __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
   },
