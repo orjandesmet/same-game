a83b1b5e15589b56a486ce720dbe9b738fb98d4f
Orjan De Smet <orjandesmet@users.noreply.github.com>
2025-12-02 00:47:50 +0100

    feat: Updated seed icon (#61)

--- src/components/SeedIcon/SeedIcon.tsx
+++ src/components/SeedIcon/SeedIcon.tsx
@@ -1,3 +1,391 @@
+import styles from './SeedIcon.module.css';
+
 export function SeedIcon() {
-  return <span>🌱</span>;
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      version="1.1"
+      className={styles['seed-icon']}
+      viewBox="0 0 16 16"
+      aria-hidden="true"
+    >
+      <g className={styles['left-leaf']}>
+        <rect width="1" height="1" x="6" y="9" fill="currentColor" />
+        <rect width="2" height="1" x="4" y="8" fill="currentColor" />
+        <rect width="1" height="1" x="3" y="7" fill="currentColor" />
+        <rect width="1" height="2" x="2" y="6" fill="currentColor" />
+        <rect width="1" height="2" x="1" y="4" fill="currentColor" />
+        <rect width="1" height="1" x="0" y="3" fill="currentColor" />
+        <rect width="5" height="1" x="0" y="2" fill="currentColor" />
+        <rect width="2" height="1" x="5" y="3" fill="currentColor" />
+        <rect width="1" height="2" x="7" y="4" fill="currentColor" />
+        <rect
+          width="1"
+          height="1"
+          x="1"
+          y="3"
+          className={styles['dark-green']}
+        />
+        <rect
+          width="3"
+          height="1"
+          x="2"
+          y="3"
+          className={styles['light-green']}
+        />
+        <rect
+          width="1"
+          height="1"
+          x="2"
+          y="4"
+          className={styles['dark-green']}
+        />
+        <rect
+          width="4"
+          height="1"
+          x="3"
+          y="4"
+          className={styles['light-green']}
+        />
+        <rect
+          width="2"
+          height="1"
+          x="2"
+          y="5"
+          className={styles['dark-green']}
+        />
+        <rect
+          width="3"
+          height="1"
+          x="4"
+          y="5"
+          className={styles['light-green']}
+        />
+        <rect
+          width="2"
+          height="1"
+          x="3"
+          y="6"
+          className={styles['dark-green']}
+        />
+        <rect
+          width="3"
+          height="1"
+          x="5"
+          y="6"
+          className={styles['light-green']}
+        />
+        <rect
+          width="2"
+          height="1"
+          x="4"
+          y="7"
+          className={styles['dark-green']}
+        />
+        <rect
+          width="2"
+          height="1"
+          x="6"
+          y="7"
+          className={styles['light-green']}
+        />
+        <rect
+          width="1"
+          height="1"
+          x="6"
+          y="8"
+          className={styles['dark-green']}
+        />
+        <rect
+          width="1"
+          height="2"
+          x="7"
+          y="8"
+          className={styles['light-green']}
+        />
+      </g>
+      <g className={styles['left-leaf-alt']}>
+        <rect width="2" height="1" x="5" y="9" fill="currentColor" />
+        <rect width="1" height="1" x="4" y="8" fill="currentColor" />
+        <rect width="1" height="2" x="3" y="6" fill="currentColor" />
+        <rect width="1" height="6" x="2" y="0" fill="currentColor" />
+        <rect width="1" height="1" x="3" y="1" fill="currentColor" />
+        <rect width="2" height="1" x="4" y="2" fill="currentColor" />
+        <rect width="1" height="1" x="6" y="3" fill="currentColor" />
+        <rect width="1" height="2" x="7" y="4" fill="currentColor" />
+        <rect
+          width="1"
+          height="1"
+          x="3"
+          y="2"
+          className={styles['light-green']}
+        />
+        <rect
+          width="1"
+          height="1"
+          x="3"
+          y="3"
+          className={styles['dark-green']}
+        />
+        <rect
+          width="2"
+          height="1"
+          x="4"
+          y="3"
+          className={styles['light-green']}
+        />
+        <rect
+          width="1"
+          height="1"
+          x="3"
+          y="4"
+          className={styles['dark-green']}
+        />
+        <rect
+          width="3"
+          height="1"
+          x="4"
+          y="4"
+          className={styles['light-green']}
+        />
+        <rect
+          width="2"
+          height="1"
+          x="3"
+          y="5"
+          className={styles['dark-green']}
+        />
+        <rect
+          width="2"
+          height="1"
+          x="5"
+          y="5"
+          className={styles['light-green']}
+        />
+        <rect
+          width="1"
+          height="1"
+          x="4"
+          y="6"
+          className={styles['dark-green']}
+        />
+        <rect
+          width="3"
+          height="1"
+          x="5"
+          y="6"
+          className={styles['light-green']}
+        />
+        <rect
+          width="2"
+          height="1"
+          x="4"
+          y="7"
+          className={styles['dark-green']}
+        />
+        <rect
+          width="2"
+          height="1"
+          x="6"
+          y="7"
+          className={styles['light-green']}
+        />
+        <rect
+          width="2"
+          height="1"
+          x="5"
+          y="8"
+          className={styles['dark-green']}
+        />
+        <rect
+          width="1"
+          height="2"
+          x="7"
+          y="8"
+          className={styles['light-green']}
+        />
+      </g>
+      <g className={styles['right-leaf']}>
+        <rect width="1" height="3" x="8" y="6" fill="currentColor" />
+        <rect width="1" height="1" x="9" y="5" fill="currentColor" />
+        <rect width="2" height="1" x="10" y="4" fill="currentColor" />
+        <rect width="4" height="1" x="12" y="3" fill="currentColor" />
+        <rect width="1" height="1" x="15" y="4" fill="currentColor" />
+        <rect width="1" height="2" x="14" y="5" fill="currentColor" />
+        <rect width="1" height="1" x="13" y="7" fill="currentColor" />
+        <rect width="2" height="1" x="11" y="8" fill="currentColor" />
+        <rect width="2" height="1" x="9" y="9" fill="currentColor" />
+        <rect
+          width="1"
+          height="1"
+          x="12"
+          y="4"
+          className={styles['dark-green']}
+        />
+        <rect
+          width="2"
+          height="1"
+          x="13"
+          y="4"
+          className={styles['light-green']}
+        />
+        <rect
+          width="2"
+          height="1"
+          x="10"
+          y="5"
+          className={styles['dark-green']}
+        />
+        <rect
+          width="2"
+          height="1"
+          x="12"
+          y="5"
+          className={styles['light-green']}
+        />
+        <rect
+          width="2"
+          height="1"
+          x="9"
+          y="6"
+          className={styles['dark-green']}
+        />
+        <rect
+          width="3"
+          height="1"
+          x="11"
+          y="6"
+          className={styles['light-green']}
+        />
+        <rect
+          width="1"
+          height="1"
+          x="9"
+          y="7"
+          className={styles['dark-green']}
+        />
+        <rect
+          width="3"
+          height="1"
+          x="10"
+          y="7"
+          className={styles['light-green']}
+        />
+        <rect
+          width="2"
+          height="1"
+          x="9"
+          y="8"
+          className={styles['dark-green']}
+        />
+      </g>
+      <g className={styles['right-leaf-alt']}>
+        <rect width="1" height="3" x="8" y="6" fill="currentColor" />
+        <rect width="1" height="1" x="9" y="5" fill="currentColor" />
+        <rect width="1" height="1" x="10" y="4" fill="currentColor" />
+        <rect width="1" height="1" x="11" y="3" fill="currentColor" />
+        <rect width="1" height="1" x="12" y="2" fill="currentColor" />
+        <rect width="1" height="5" x="13" y="1" fill="currentColor" />
+        <rect width="1" height="2" x="12" y="6" fill="currentColor" />
+        <rect width="1" height="1" x="11" y="8" fill="currentColor" />
+        <rect width="2" height="1" x="9" y="9" fill="currentColor" />
+        <rect
+          width="1"
+          height="1"
+          x="12"
+          y="3"
+          className={styles['light-green']}
+        />
+        <rect
+          width="1"
+          height="1"
+          x="11"
+          y="4"
+          className={styles['dark-green']}
+        />
+        <rect
+          width="1"
+          height="1"
+          x="12"
+          y="4"
+          className={styles['light-green']}
+        />
+        <rect
+          width="1"
+          height="1"
+          x="10"
+          y="5"
+          className={styles['dark-green']}
+        />
+        <rect
+          width="2"
+          height="1"
+          x="11"
+          y="5"
+          className={styles['light-green']}
+        />
+        <rect
+          width="2"
+          height="1"
+          x="9"
+          y="6"
+          className={styles['dark-green']}
+        />
+        <rect
+          width="1"
+          height="1"
+          x="11"
+          y="6"
+          className={styles['light-green']}
+        />
+        <rect
+          width="1"
+          height="1"
+          x="9"
+          y="7"
+          className={styles['dark-green']}
+        />
+        <rect
+          width="2"
+          height="1"
+          x="10"
+          y="7"
+          className={styles['light-green']}
+        />
+        <rect
+          width="2"
+          height="1"
+          x="9"
+          y="8"
+          className={styles['dark-green']}
+        />
+      </g>
+      <g className={styles['stem']}>
+        <rect width="1" height="1" x="6" y="15" fill="currentColor" />
+        <rect width="1" height="5" x="7" y="10" fill="currentColor" />
+        <rect width="1" height="6" x="9" y="10" fill="currentColor" />
+        <rect
+          width="1"
+          height="2"
+          x="8"
+          y="9"
+          className={styles['light-green']}
+        />
+        <rect
+          width="1"
+          height="5"
+          x="8"
+          y="11"
+          className={styles['dark-green']}
+        />
+        <rect
+          width="1"
+          height="1"
+          x="7"
+          y="15"
+          className={styles['dark-green']}
+        />
+      </g>
+    </svg>
+  );
 }
